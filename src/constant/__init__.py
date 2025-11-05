75d4a0ec17eed8922e7910cc86d1264194b75658
Akshat Verma <vermaakshat628@gmail.com>
2025-11-05 13:18:41 +0530

    final update

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,17 +1,18 @@
 import os
-from urllib.parse import quote_plus
+# from urllib.parse import quote_plus
 
-AWS_S3_BUCKET_NAME = "wafer-fault"
+AWS_S3_BUCKET_NAME = "akshat628"
 MONGO_DATABASE_NAME = "akshat"
 MONGO_COLLECTION_NAME = "waferfault"
 
 
 TARGET_COLUMN = "quality"
-username = "vermaakshat628"
-password = quote_plus("Ajtdmws@1")  # encodes the '@'
+# username = "vermaakshat628"
+# password = quote_plus("Ajtdmws@1")  # encodes the '@'
 
 #url
-MONGO_DB_URL= f"mongodb+srv://{username}:{password}@cluster0.kvdlevi.mongodb.net/?appName=Cluster0"
+# MONGO_DB_URL= f"mongodb+srv://{username}:{password}@cluster0.kvdlevi.mongodb.net/?appName=Cluster0"
+MONGO_DB_URL="mongodb+srv://vermaakshat628:Ajtdmwsa1@cluster0.kvdlevi.mongodb.net/?appName=Cluster0"
 
 
 MODEL_FILE_NAME = "model"
